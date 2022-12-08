fca5a81ee0ce4f568e898bf3986e33bb783fc8bf
aneal0 <113207474+aneal0@users.noreply.github.com>
2022-12-08 11:58:22 -0500

    Update

---
+++ Project 3/main.js
@@ -0,0 +1,19 @@
+// const btn = document.getElementById('button5');
+
+// btn.addEventListener('click', () => {
+//   const form = document.getElementById('form');
+
+//   if (form.style.visibility === 'hidden') {
+//     form.style.visibility = 'visible';
+//   } else {
+//     form.style.visibility = 'hidden';
+//   }
+// });
+
+
+function myFunction() {
+  // var element = document.body;
+  // element.classList.toggle("light");
+
+  $('button5').toggleClass('light');
+}
